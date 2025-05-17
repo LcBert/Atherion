427323f316c599d282780d5ec73c783462d9d1d3
Luca Bertaggia <luca.bertaggia82@gmail.com>
2025-05-18 00:38:07 +0200

    test

---
+++ kubejs/server_scripts/experience_scoreboard/experience_scoreboard.js
@@ -0,0 +1,5 @@
+PlayerEvents.tick(event => {
+    const scoreboard = event.server.getScoreboard();
+    // event.player.tell(event.player.getScoreboard().get)
+    // event.player.tell(event.player.getScoreboard().getObjectiveNames(event.player.getScoreboard().getObjective("experience")).toArray());
+})
