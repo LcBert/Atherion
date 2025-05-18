2197153a6ca566905616538cd7f13abcc8021675
Fededad <federico.franciosi17@gmail.com>
2025-05-18 19:13:45 +0200

    Xp Overlay Fix

--- kubejs/client_scripts/xpOverlay.js
+++ kubejs/client_scripts/xpOverlay.js
@@ -1,24 +1,40 @@
 ClientEvents.paintScreen(event => {
     const screen_x = event.width
     const screen_y = event.height
+
+    const player = Client.player
+    const currentScreen = Client.screen
+
     const overlay = {
         xp_overlay: {
             type: "text",
             text: "0",
-            color: "#00FF00",
+            color: "#65FF3A",
             shadow: true,
             alignX: "center",
             alignY: "bottom",
             moveY: -25,
             draw: "always"
         }
+    };
+
+    if (currentScreen) {
+        let screenClass = currentScreen.getClass()
+
+        if (screenClass) {
+            overlay.xp_overlay.color = "#253B12"
+        }
     }
 
-    if (event.player.hasContainerOpen())
-        overlay.xp_overlay.color = "#BB00FF00"
-    // overlay.xp_overlay.color = "#BBFF0000"
+    try {
+        event.player.paint({ xp_overlay: { remove: true } })
+    } catch (e) {}
 
-    try { event.player.paint({ xp_overlay: { remove: true } }) } catch (e) { }
-    if (event.player.getXpLevel() == 0 && !event.player.isCreative() && !event.player.isSpectator())
+    if (
+        event.player.getXpLevel() === 0 &&
+        !event.player.isCreative() &&
+        !event.player.isSpectator()
+    ) {
         event.player.paint(overlay)
-})
+    }
+});
