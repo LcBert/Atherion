9b5a66c7dbeb6b5b6e0dc594ec33473a3e507c8e
Luca Bertaggia <luca.bertaggia82@gmail.com>
2025-06-17 11:15:04 +0200

    Items and blocks protection for all classes

--- kubejs/server_scripts/class_protection/blocks.js
+++ kubejs/server_scripts/class_protection/blocks.js
@@ -1,15 +1,18 @@
 BlockEvents.rightClicked(event => {
     let is_entered = false
     let can_open = false
+    let classes = ["alchemist", "archer", "bard", "blacksmith", "carpenter", "chef", "farmer", "mage", "miner", "shadow", "warrior"]
 
-    for (let i = 0; i <= 4; i++) {
-        if (event.getBlock().hasTag("atherion:usable/blacksmith/" + i)) {
-            is_entered = true
-            for (let j = i; j <= 4; j++)
-                if (event.getPlayer().getNbt().getAsString().search("origins:class/blacksmith/blacksmith" + j) >= 0)
-                    can_open = true
+    classes.forEach(_class => {
+        for (let i = 0; i <= 4; i++) {
+            if (event.getBlock().hasTag("atherion:usable/" + _class + "/" + i)) {
+                is_entered = true
+                for (let j = i; j <= 4; j++)
+                    if (event.getPlayer().getNbt().getAsString().search("origins:class/" + _class + "/" + _class + "" + j) >= 0)
+                        can_open = true
+            }
         }
-    }
+    })
 
     if (is_entered && !can_open)
         event.cancel()
--- kubejs/server_scripts/class_protection/items.js
+++ kubejs/server_scripts/class_protection/items.js
@@ -1,15 +1,18 @@
 PlayerEvents.tick(event => {
     let is_entered = false
     let can_use = false
+    let classes = ["alchemist", "archer", "bard", "blacksmith", "carpenter", "chef", "farmer", "mage", "miner", "shadow", "warrior"]
 
-    for (let i = 0; i <= 4; i++) {
-        if (event.getPlayer().mainHandItem.hasTag("atherion:usable/blacksmith/" + i)) {
-            is_entered = true
-            for (let j = i; j <= 4; j++)
-                if (event.getPlayer().getNbt().getAsString().search("origins:class/blacksmith/blacksmith" + j) >= 0)
-                    can_use = true
+    classes.forEach(_class => {
+        for (let i = 0; i <= 4; i++) {
+            if (event.getPlayer().mainHandItem.hasTag("atherion:usable/" + _class + "/" + i)) {
+                is_entered = true
+                for (let j = i; j <= 4; j++)
+                    if (event.getPlayer().getNbt().getAsString().search("origins:class/" + _class + "/" + _class + "" + j) >= 0)
+                        can_use = true
+            }
         }
-    }
+    })
 
     if (is_entered && !can_use) {
         event.player.runCommandSilent(`/effect give @s minecraft:weakness 1 50 true`)
